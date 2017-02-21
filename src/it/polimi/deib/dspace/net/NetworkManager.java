99cbfd4c750c8a03952cdc361af27e5943e567a0
arlind1992 <arlind_rufi@hotmail.com>
2017-02-21 15:41:15 +0000

    modificato network manager

--- src/it/polimi/deib/dspace/net/NetworkManager.java
+++ src/it/polimi/deib/dspace/net/NetworkManager.java
@@ -11,16 +11,32 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import it.polimi.deib.dspace.control.Configuration;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -33,6 +49,7 @@ public class NetworkManager {
 	private static String rootEndpoint = "http://localhost:8000";
 	private static String vmConfigsEndpoint = "http://localhost:8080/vm-types";
 	private static String modelUploadEndpoint = rootEndpoint+"/files/view/upload";
+	private static String uploadRest=rootEndpoint+"/files/upload";
 	public static NetworkManager getInstance(){
 		if(instance != null){
 			return instance;
@@ -81,7 +98,7 @@ public class NetworkManager {
 	 * @param scenario The scenario parameter
 	 * @throws UnsupportedEncodingException 
 	 */
-	public void sendModel(List<File> files, String scenario) throws UnsupportedEncodingException{
+	/*public void sendModel(List<File> files, String scenario) throws UnsupportedEncodingException{
 		HttpClient httpclient = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
 		HttpResponse response;
 		HttpPost post = new HttpPost(modelUploadEndpoint);
@@ -94,6 +111,7 @@ public class NetworkManager {
 	    post.setEntity(builder.build());
 	    try {
 	    	response = httpclient.execute(post);
+	    	System.out.println(response.toString());
 			if(response.getStatusLine().getStatusCode() != 302){
 				System.err.println("Error: POST not succesfull");
 			}
@@ -104,11 +122,100 @@ public class NetworkManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}*/
+	
+	
+	/**
+	 * Sends to the backend the models to be simulated
+	 * @param files The model files
+	 * @param scenario The scenario parameter
+	 * @throws UnsupportedEncodingException 
+	 */
+	
+	public void sendModel(List<File> files, String scenario) throws UnsupportedEncodingException{
+		HttpClient httpclient =HttpClients.createDefault();
+		HttpResponse response;
+		HttpPost post = new HttpPost(this.uploadRest);
+		
+		MultipartEntityBuilder builder = MultipartEntityBuilder.create();  
+		builder.addPart("scenario",new StringBody(scenario,ContentType.DEFAULT_TEXT));
+		for(File file:files){
+			builder.addPart("file[]", new FileBody(file));
+		}
+	    post.setEntity(builder.build());
+	    try {
+	    	response = httpclient.execute(post);
+	    	String json = EntityUtils.toString(response.getEntity());
+	    	HttpPost repost=new HttpPost(this.getLink(json));
+	    	response=httpclient.execute(repost);
+	    	String js = EntityUtils.toString(response.getEntity());
+	    	parseJson(js);
+	    	if(response.getStatusLine().getStatusCode() != 302){
+				System.err.println("Error: POST not succesfull");
+			}
+			else{
+				//response.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  
+	    
 	}
+
+	
 
 	public String[] getTechnologies(){
 		String s[] = {"Storm", "MapReduce", "Hadoop"};
 		return s;
+	}
+	
+	private void parseJson(String string){
+		JSONParser parser = new JSONParser();
+		try {
+			JSONObject json = (JSONObject) parser.parse(string);
+			JSONObject lin=(JSONObject) json.get("_links");
+			JSONObject sub=(JSONObject) lin.get("solution");
+			String link=(String) sub.get("href");
+			File f = new File("results");
+			if(!f.exists()) { 
+				try{
+				    PrintWriter writer = new PrintWriter("results", "UTF-8");
+				    writer.println(Configuration.getCurrent().getID());
+				    writer.println(link);
+				    writer.close();
+				} catch (IOException e) {
+				   // do something
+				}
+				
+			}else{
+
+				Writer output;
+				output = new BufferedWriter(new FileWriter("results",true));  //clears file every time
+				output.append(Configuration.getCurrent().getID());
+				output.append(link);
+				output.close();
+			}
+			
+		} catch (ParseException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private String getLink(String string){
+		JSONParser parser = new JSONParser();
+		String link="";
+		try {
+			JSONObject json = (JSONObject) parser.parse(string);
+			JSONObject lin=(JSONObject) json.get("_links");
+			JSONObject sub=(JSONObject) lin.get("submit");
+			link=(String) sub.get("href");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return link;
 	}
 	
 }
